ded3a7ce3a8116c7a62c2f329a42e7387b06296d
Pablo Gutierrez <pablguti.pg@gmail.com>
2022-11-07 09:55:53 -0300

    .

---
+++ app/src/main/java/com/example/pavainteligente/Pava.java
@@ -0,0 +1,51 @@
+package com.example.pavainteligente;
+
+public class Pava {
+
+    private String color;
+    private String name;
+    private String status;
+    private String temperature;
+    private String switchStatus;
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getSwitchStatus() {
+        return switchStatus;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setSwitchStatus(String switchStatus) {
+        this.switchStatus = switchStatus;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+}
